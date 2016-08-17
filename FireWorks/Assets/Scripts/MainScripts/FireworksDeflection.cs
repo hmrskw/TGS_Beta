05ff4499071b147cecb929d6ce1d1df43910f540
岩野耀嵩 <a.lb.3823cm@gmail.com>
2016-08-17 18:27:28 +0900

    デザイナーから提出された画像をタイトルのボタン画像に適用

--- FireWorks/Assets/Scripts/MainScripts/FireworksDeflection.cs
+++ FireWorks/Assets/Scripts/MainScripts/FireworksDeflection.cs
@@ -3,18 +3,14 @@ using System.Collections;
 
 public class FireworksDeflection : MonoBehaviour
 {
-    [SerializeField,Tooltip("花火が打ちあがるときの振れ幅。0番目がマイナスで１番目がプラス")]
-    private float[] deflectionWidth = new float[2];
-
-	// Use this for initialization
-	void Start ()
-    {
-	
-	}
-	
+    [SerializeField, Tooltip("花火が打ちあがるときの振れ幅。")]
+    private float moveSpeed;
+    
 	// Update is called once per frame
 	void Update ()
     {
-	
-	}
+        transform.position = new Vector3(this.transform.position.x + Mathf.Sin(Time.frameCount * moveSpeed), 
+                                         this.transform.position.y, 
+                                         this.transform.position.z);
+    }
 }
---
+++ FireWorks/Assets/Scripts/TitleScripts/ChangeButtonIconSprite.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class ChangeButtonIconSprite : MonoBehaviour
+{
+    enum ButtonIcon
+    {
+        HOLD_HAND = 0,
+        OPEN_HAND
+    }
+
+    const int ICON_NUM = 2;
+
+    ButtonIcon buttonIconNum = ButtonIcon.HOLD_HAND;
+
+    [SerializeField]
+    Sprite[] buttonIcon = new Sprite[ICON_NUM];
+
+    private Image viewIcon;
+    
+    //カウントする変数
+    private int changeCountNum = 0;
+
+    //1秒
+    private const int CHANGE_TIME = 60;
+
+    //画像を変えるかどうか
+    private bool canChange = false;
+
+
+    void Start()
+    {
+        Debug.Log("kita");
+
+
+        viewIcon = GetComponent<Image>();
+        
+        viewIcon.sprite = buttonIcon[0];
+
+        StartCoroutine(SpriteChangeTimer());
+    }
+
+    IEnumerator SpriteChangeTimer()
+    {
+        Debug.Log("kita2");
+
+        while (true)
+        {
+            viewIcon.sprite = buttonIcon[1];
+
+            yield return new WaitForSeconds(1.0f);
+
+            viewIcon.sprite = buttonIcon[0];
+
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
