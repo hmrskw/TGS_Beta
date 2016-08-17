1613662e3b8c478c4eb5bc5ed43ea1493cec28c9
岩野耀嵩 <a.lb.3823cm@gmail.com>
2016-08-17 15:32:20 +0900

    CSVデータの仕様変更。花火のサイズ設定を追加。
    花火打ち上げ時の振れ幅を作るコードを製作中

---
+++ FireWorks/Assets/Scripts/MainScripts/FireworksDeflection.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireworksDeflection : MonoBehaviour
+{
+    [SerializeField,Tooltip("花火が打ちあがるときの振れ幅。0番目がマイナスで１番目がプラス")]
+    private float[] deflectionWidth = new float[2];
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
